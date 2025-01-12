a174570e555c9aad09d56295aa51ddbf80e02267
AryaaSk <aryaa_saravana@outlook.com>
2025-01-12 17:54:02 +0000

    Iteration 1 complete; create simple drawing canvas

--- .JS/script.js
+++ .JS/script.js
@@ -1,26 +1,23 @@
+"use strict";
 const canvas = new Canvas();
 canvas.linkCanvas("canvas");
-const element: HTMLCanvasElement = canvas.canvas;
-
+const element = canvas.canvas;
 let mouseDown = false;
 element.onmousedown = () => {
     mouseDown = true;
-}
+};
 element.onmouseup = () => {
     mouseDown = false;
-}
-
-element.onmousemove = ($e: MouseEvent) => {
+};
+element.onmousemove = ($e) => {
     if (mouseDown == false) {
         return;
     }
-
     const [offsetX, offsetY] = [$e.offsetX, $e.offsetY];
     const [x, y] = [canvas.GridX(offsetX), canvas.GridY(offsetY)];
     canvas.plotPoint([x, y], "black");
-}
-
-const clearButton = document.getElementById("clearButton")!;
+};
+const clearButton = document.getElementById("clearButton");
 clearButton.onclick = () => {
     canvas.clearCanvas();
-}
+};
