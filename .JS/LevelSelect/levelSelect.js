908c3c3909188ada1c8b28284bcd504f283540e8
AryaaSk <aryaa_saravana@outlook.com>
2025-01-22 13:57:02 +0000

    level select collection view complete

--- .JS/LevelSelect/levelSelect.js
+++ .JS/LevelSelect/levelSelect.js
@@ -1,1 +1,16 @@
 "use strict";
+//to center collection view
+const ResizeGrid = (numCells) => {
+    //Add grid columns to the Activity Grid
+    let levelWidth = Number(getComputedStyle(document.body).getPropertyValue('--activityWidth').slice(0, -2));
+    if (levelWidth == 0 || levelWidth == undefined) {
+        levelWidth = 100; //safe guard
+    }
+    let gridColumns = Math.floor((window.innerWidth - 50) / levelWidth);
+    if (gridColumns > numCells) {
+        gridColumns = numCells;
+    }
+    let repeatProperty = `repeat(${gridColumns}, ${levelWidth}px)`;
+    document.getElementById("main").style.gridTemplateColumns = repeatProperty;
+};
+ResizeGrid(5);
---
+++ Src/LevelSelect/levelSelect.ts
@@ -0,0 +1,17 @@
+//to center collection view
+const ResizeGrid = (numCells: number) => {
+    //Add grid columns to the Activity Grid
+    let levelWidth = Number(getComputedStyle(document.body).getPropertyValue('--activityWidth').slice(0, -2));
+    if (levelWidth == 0 || levelWidth == undefined) {
+        levelWidth = 100; //safe guard
+    }
+
+    let gridColumns = Math.floor((window.innerWidth - 50) / levelWidth);
+    if (gridColumns > numCells) {
+        gridColumns = numCells;
+    }
+    let repeatProperty = `repeat(${gridColumns}, ${levelWidth}px)`
+    document.getElementById("main")!.style.gridTemplateColumns = repeatProperty;
+}
+
+ResizeGrid(5)
